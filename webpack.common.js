f44c77853635c4ce8ec5d09ed2b1ab053f9992fa
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-11 19:26:09 +0700

    Integrate CopyWebpackPlugin for asset copying
    Add CopyWebpackPlugin to copy public and model directories

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
       scriptLoading: "module",
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src/public"), to: path.resolve(__dirname, "dist") },
+        { from: path.resolve(__dirname, "src/model"), to: path.resolve(__dirname, "dist/model") }
+     ]
+   })
   ],
   stats: {
     warningsFilter: /import\.meta/,
