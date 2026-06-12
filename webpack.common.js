f776ae1f9240f2b8a26618e1942c912d2c20bad5
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-12 18:49:58 +0700

    Add CopyWebpackPlugin to copy public and model files

--- webpack.common.js
+++ webpack.common.js
@@ -42,6 +42,13 @@ module.exports = {
       template: path.resolve(__dirname, "src/index.html"),
       scriptLoading: "module",
     })
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src/public"), to: path.resolve(__dirname, "dist") },
+        { from: path.resolve(__dirname, "src/model"), to: path.resolve(__dirname, "dist/model") },
+      ],
+    }),
   ],
   stats: {
     warningsFilter: /import\.meta/,
