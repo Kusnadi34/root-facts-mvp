c0f0ab6cd5305111e5b75d9ac2eed3fef57c8207
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-12 19:00:03 +0700

    Update Workbox plugin configuration in webpack.prod.js
    Replace Workbox GenerateSW with InjectManifest for custom service worker.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const common = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
@@ -20,28 +21,11 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
     }),
-    new WorkboxWebpackPlugin.GenerateSW({
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: path.resolve(__dirname, "src/sw.js"),
       swDest: "sw.js",
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      skipWaiting: true,
-      clientsClaim: true,
-      
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/api\./i,
-          handler: "NetworkFirst",
-          options: {
-            cacheName: "api-cache",
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 60 * 60 * 24,
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
+      globPatterns: ["**/*.{html,css,js,json,png,jpg,jpeg,bin}"],
     }),
   ],
 });
