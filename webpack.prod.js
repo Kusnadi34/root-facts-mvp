af95c0d0dd821c05d00e1a8abdffff3945f48f9c
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-12 19:20:48 +0700

    Refactor WorkboxWebpackPlugin for service worker generation
    Updated WorkboxWebpackPlugin configuration to use GenerateSW and modified caching settings.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,6 @@ const common = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
-const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
@@ -21,11 +20,24 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
     }),
-    new WorkboxWebpackPlugin.InjectManifest({
-      swSrc: path.resolve(__dirname, "src/sw.js"),
+    new WorkboxWebpackPlugin.GenerateSW({
       swDest: "sw.js",
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      globPatterns: ["**/*.{html,css,js,json,png,jpg,jpeg,bin}"],
+      maximumFileSizeToCacheInBytes: 15 * 1024 * 1024,
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/api\./i,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+      ],
     }),
   ],
 });
