c36333de38c5ffb45fcb20eb5a8e9c31f4b3eeff
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-18 15:42:28 +0700

    Replace InjectManifest with GenerateSW in Webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,8 +20,7 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
     }),
-    new WorkboxWebpackPlugin.InjectManifest({
-      swSrc: path.resolve(__dirname, "src/sw.js"),
+    new WorkboxWebpackPlugin.GenerateSW({
       swDest: "sw.js",
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       skipWaiting: true,
