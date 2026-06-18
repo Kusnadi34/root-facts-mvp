b172210e188417a1653926daf5dae9ab63f466fa
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-18 14:32:02 +0700

    Refactor service worker code and update syntax

--- src/sw.js
+++ src/sw.js
@@ -1,36 +1,19 @@
-import { precacheAndRoute } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
-import { NetworkFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
 
 precacheAndRoute(self.__WB_MANIFEST);
-
-
 registerRoute(
   /^https:\/\/api\./i,
-  new NetworkFirst({
-    cacheName: "api-cache",
-    expiration: {
-      maxEntries: 50,
-      maxAgeSeconds: 24 * 60 * 60,
-    },
-  })
+  new NetworkFirst({ cacheName: 'api-cache' })
 );
 
 
 registerRoute(
   /^https:\/\/fonts\.googleapis\.com/,
-  new StaleWhileRevalidate({
-    cacheName: "google-fonts",
-  })
+  new StaleWhileRevalidate({ cacheName: 'google-fonts' })
 );
 
-
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(clients.claim());
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
