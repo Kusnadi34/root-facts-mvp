7787f951169ffd87874a82384f95eae94902edfd
Kusnadi34 <nanakusnadi035@gmail.com>
2026-06-19 23:11:12 +0700

    Import CopyWebpackPlugin and path in webpack.prod.js
    Added CopyWebpackPlugin and path module imports.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,8 @@ const common = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: "production",
